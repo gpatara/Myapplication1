bc1479241e062df548819499dd3e0171caf80793
Admin <givi.patiashvili.1@btu.edu.ge>
2019-11-15 17:21:37 +0400

    test

--- app/src/main/java/com/example/givi/MainActivity.kt
+++ app/src/main/java/com/example/givi/MainActivity.kt
@@ -1,27 +1,27 @@
 // Interface
-internal interface Animal {
-    fun animalSound()  // interface method (does not have a body)
-    fun sleep()  // interface method (does not have a body)
-}
-
-// Pig "implements" the Animal interface
-internal class Pig : Animal {
-    override fun animalSound() {
-        // The body of animalSound() is provided here
-        println("The pig says: wee wee")
+    internal interface Animal {
+        fun animalSound()  // interface method (does not have a body)
+        fun sleep()  // interface method (does not have a body)
     }
 
-    override fun sleep() {
-        // The body of sleep() is provided here
-        println("Zzz")
-    }
-}
+    // Pig "implements" the Animal interface
+    internal class Pig : Animal {
+        override fun animalSound() {
+            // The body of animalSound() is provided here
+            println("The pig says: wee wee")
+        }
 
-internal object MyMainClass {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val myPig = Pig()  // Create a Pig object
-        myPig.animalSound()
-        myPig.sleep()
+        override fun sleep() {
+            // The body of sleep() is provided here
+            println("Zzz")
+        }
     }
+
+    internal object MyMainClass {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val myPig = Pig()  // Create a Pig object
+            myPig.animalSound()
+            myPig.sleep()
+        }
 }
